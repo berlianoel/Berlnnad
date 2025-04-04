97a9f77da4e931b0be63841522cb3351d7dd7476
berliano <107350669+berlianoel@users.noreply.github.com>
2025-04-04 19:27:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# -- keep_alive code added below --
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+# -- end keep_alive code --
+
+from flask import Flask
+from threading import Thread
+
+# -- user script begins --
 import logging
 import time
 import random
